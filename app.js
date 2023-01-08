f892a54f3b3535f4c58156a2ef872f366f2cba08
jeet shekhaliya <51267357+whitestorm007@users.noreply.github.com>
2023-01-08 12:16:41 +0530

    main

---
+++ app.js
@@ -0,0 +1,62 @@
+var pathToFfmpeg = require('ffprobe-static');
+const { execFile } = require('child_process')
+
+
+
+/**
+ * Executes ffprobe with provided arguments
+ * @func    ffprobeExecFile
+ * @param   {String}        path Path of the ffprobe binary
+ * @param   {Array<String>} args Array of arguments passed to ffprobe
+ * @returns {Promise<Object>}    Promise that resolves to the ffprobe JSON output
+ */
+function ffprobeExecFile(path, args) {
+
+    return new Promise((resolve, reject) => {
+        execFile(path, args, (err, stdout, stderr) => {
+            if (err) {
+                console.log(err);
+                if (err.code === 'ENOENT') {
+                    reject(err)
+                } else {
+                    const ffprobeErr = new Error(stderr.split('\n').pop())
+                    console.log(ffprobeErr);
+                    reject(ffprobeErr)
+                }
+            } else {
+                resolve(JSON.parse(stdout))
+            }
+        })
+    })
+}
+
+/**
+ * Analyzes a video with ffprobe
+ * @func    ffprobe
+ * @param   {String} target   The file path or remote URL of the video
+ * @param   {Object} [config={}]             A configuration object
+ * @param   {String} [config.path='ffprobe'] Path of the ffprobe binary
+ * @returns {Promise<Object>} Promise that resolves to the ffprobe JSON output
+ */
+
+function ffprobe(target, config = {}) {
+    const path = pathToFfmpeg.path //"./ffprobe/ffprobe"
+    const args = [
+        '-show_streams',
+        '-show_format',
+        '-print_format',
+        'json',
+        target
+    ]
+
+
+    return ffprobeExecFile(path, args)
+}
+
+
+console.log(pathToFfmpeg);
+
+async function test() {
+    console.log(await ffprobe("https://seach.sigmacdn1.workers.dev/stream/782167627762213921326932754d4e52605250363664523b697b67733353506a347231424c574a625a75212f21776a6e6621393235343032363a3631373235367e/Money%20Heist%20S01-%20Episode%2001.mkv"));
+}
+test()
